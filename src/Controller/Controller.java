d9db002b923f03f3a601f747450f11279aeb3d81
Gustavo Vaz Ferreira <gustavovaz278@gmail.com>
2025-08-10 17:05:02 -0300

    Atualizaão
    Atualização

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,6 +1,13 @@
 package Controller;
 
+import javax.swing.JOptionPane;
+
 public class Controller {
+	public static String Entrada() {
+		String Texto = JOptionPane.showInputDialog
+				("Digite as palavras separadas por ;");
+		return Texto;
+	}
 
 	public void Texto(String frase) {
 		String[] Vpalavra = frase.split(";");
--- src/View/principal.java
+++ src/View/principal.java
@@ -7,21 +7,15 @@ import Controller.Controller;
 public class principal {
 
 	public static void main(String[] args) {
-		Controller op = new Controller(); 
-		String Texto1 = JOptionPane.showInputDialog("Digite as palavras separadas por ;");
-		String Texto2 = JOptionPane.showInputDialog("Digite as palavras separadas por ;");
-		String Texto3 = JOptionPane.showInputDialog("Digite as palavras separadas por ;");
-		// Texto1 =
-		// ("abóbora;abobrinha;alcachofra;aspargos;batata-doce;berinjela;beterraba");
-		// Texto2 =
-		// ("abacate;ameixa;amora;banana;cajá;figo;maçã;melancia;uva;seriguela;manga");
-		// Texto3 =
-		// ("acelga;alface;alho-poró;coentro;endívia;escarola;repolho;rúcula;agrião");
+		Controller op = new Controller();
+		String Texto = op.Entrada();
 
-		op.Texto(Texto1);
-		op.Texto(Texto2);
-		op.Texto(Texto3);
+		op.Texto(Texto);
 
 	}
 
 }
+
+// Texto1 = ("abóbora;abobrinha;alcachofra;aspargos;batata-doce;berinjela;beterraba");
+// Texto2 = ("abacate;ameixa;amora;banana;cajá;figo;maçã;melancia;uva;seriguela;manga");
+// Texto3 = ("acelga;alface;alho-poró;coentro;endívia;escarola;repolho;rúcula;agrião");
